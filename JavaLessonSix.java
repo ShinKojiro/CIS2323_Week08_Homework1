1f706b9b7d6d5fb007b6807f9ab11749e295eddb
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2018-10-15 15:16:48 -0500

    added file

---
+++ JavaLessonSix.java
@@ -0,0 +1,13 @@
+public class JavaLessonSix{
+    public static void main(String[] args){
+        divideByZero(2);
+    }
+    public static void divideByZero(int a){
+
+        try {
+            System.out.println(a/0)
+        } catch (ArithmeticException e) {
+            System.out.println("You cant do that");
+        }
+    }
+}
